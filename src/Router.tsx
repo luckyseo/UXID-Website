450aa3e894cdfcd8ca22a27286a5ca80c71d3877
StarStriker <77044214+luckyseo@users.noreply.github.com>
2025-08-03 23:43:10 +1000

    router configured

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,31 @@
 // THIS IS V6
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-import Layout from "./routes/Layout";
+import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Client from "./pages/Client";
 import Mentor from "./pages/Mentor";
 import Student from "./pages/Student";
 
-import About from "./pages/About";
 import News from "./pages/News";
+import Events from "./pages/Events";
+
+import Join from "./pages/Join";
+import Sponsor from "./pages/Sponsor";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/projectUX/Client" element={<Client></Client>} />
-          <Route path="/projectUX/Mentor" element={<Mentor></Mentor>} />
-          <Route path="/projectUX/Student" element={<Student></Student> }/>
-          <Route path="/News" element={<News></News>} />
-          <Route path="/About" element={<About></About>} />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/projectUX/Client" element={<Client />} />
+          <Route path="/projectUX/Mentor" element={<Mentor />} />
+          <Route path="/projectUX/Student" element={<Student />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/news/events" element={<Events />} />
+          <Route path="/about/join" element={<Join />} />
+          <Route path="/about/sponsor" element={<Sponsor />} />
         </Route>
       </Routes>
     </BrowserRouter>
